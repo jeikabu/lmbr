caf01d69acc45ed3d6b0df3513d6b1cb95ff80ba
Jake <jeikabu@users.noreply.github.com>
2019-08-12 12:31:32 +0300

    Generate more of AzCore
    - See https://github.com/rust-lang/rust-bindgen/issues/1608

--- lmbr_sys/wrapper_fw_azcore.hpp
+++ lmbr_sys/wrapper_fw_azcore.hpp
@@ -1,5 +1,4 @@
-#define WIN32_LEAN_AND_MEAN
-//#include <AzCore/Debug/Trace.h>
-//#include <AzCore/EBus/EBus.h>
+#include <AzCore/Debug/Trace.h>
+#include <AzCore/EBus/EBus.h>
 #include <AzCore/RTTI/RTTI.h>
-//#include <AzCore/Slice/SliceComponent.h>
+#include <AzCore/Slice/SliceComponent.h>
