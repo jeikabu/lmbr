55844c843e9a1cb2f33d573ed3f2751448573756
jeikabu <jeikabu@users.noreply.github.com>
2019-08-23 20:16:04 +0300

    Editor plugin finally working
    - Needed to link against own copy of CryCommon instead of EditorCommon
    which ended up being the dll- the same loaded by Editor itself

--- editor_plugin/build.rs
+++ editor_plugin/build.rs
@@ -5,18 +5,11 @@ fn main() {
     // Check output of `cargo build --verbose`, should see something like:
     // -L native=/path/runng/target/debug/build/runng-sys-abc1234/out
     // That contains output from cmake
-    // println!(
-    //     "cargo:rustc-link-search=native={}",
-    //     dst.join("lib").display()
-    // );
-    // println!(
-    //     "cargo:rustc-link-search=native={}",
-    //     dst.join("lib64").display()
-    // );
     let config = build_config();
     let config_3rdparty = if config == BuildConfig::Debug { "debug" } else { "release" };
     let ly_root = PathBuf::from(lmbr_root().unwrap());
-    let bintemp_fw = ly_root.join("dev/BinTemp").join(bintemp_dir()).join("Code/Framework");
+    let bintemp = ly_root.join("dev/BinTemp").join(bintemp_dir());
+    let bintemp_fw = bintemp.join("Code/Framework");
     println!(
         "cargo:rustc-link-search=native={}",
         bintemp_fw.join("AzCore/AzCore").display()
@@ -32,12 +25,13 @@ fn main() {
         ly_root.join(r"3rdParty\zlib\1.2.8-pkg.2\build\win_x64\vc140").join(config_3rdparty).display()
     );
     println!("cargo:rustc-link-search=native={}",
-        ly_root.join("dev").join(bin_dir()).display()
+        bintemp.join("Code/CryEngine/CryCommon").display()
     );
     
     println!("cargo:rustc-link-lib=static=AzCore");
     println!("cargo:rustc-link-lib=static=AzFramework");
     println!("cargo:rustc-link-lib=static=lua");
     println!("cargo:rustc-link-lib=static=zlib");
-    println!("cargo:rustc-link-lib=static=EditorCommon");
+    println!("cargo:rustc-link-lib=static=CryCommonLib");
+    println!("cargo:rustc-link-lib=static=user32");
 }
