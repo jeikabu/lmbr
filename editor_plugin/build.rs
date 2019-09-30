f313406514bc9eb7be537bb2435ebcd5ff20b394
jeikabu <jeikabu@users.noreply.github.com>
2019-09-30 16:27:50 +0300

    Updates for Lumberyard 1.21 and remove unused lmbr_editor crate

--- editor_plugin/build.rs
+++ editor_plugin/build.rs
@@ -2,9 +2,6 @@ use lmbr_build::*;
 use std::{path::PathBuf};
 
 fn main() {
-    // Check output of `cargo build --verbose`, should see something like:
-    // -L native=/path/runng/target/debug/build/runng-sys-abc1234/out
-    // That contains output from cmake
     let config = build_config();
     let config_3rdparty = if config == BuildConfig::Debug { "debug" } else { "release" };
     let ly_root = PathBuf::from(lmbr_root().unwrap());
@@ -24,11 +21,14 @@ fn main() {
     println!("cargo:rustc-link-search=native={}",
         ly_root.join(r"3rdParty\zlib\1.2.8-pkg.2\build\win_x64\vc140").join(config_3rdparty).display()
     );
+    // println!("cargo:rustc-link-search=native={}",
+    //     bintemp.join("Code/CryEngine/CryCommon").display()
+    // );
     println!("cargo:rustc-link-search=native={}",
-        bintemp.join("Code/CryEngine/CryCommon").display()
+        bintemp.join("Code/Sandbox/EditorRustPlugin").display()
     );
     println!("cargo:rustc-link-search=native={}",
-        bintemp.join("Code/Sandbox/Editor").display()
+        ly_root.join("dev").join(bin_dir()).display()
     );
     
     println!("cargo:rustc-link-lib=static=AzCore");
